a716c1f351fcdcc20ad3cc1ddda40341e5c65fa9
Basma khan <basmakhan672@gmail.com>
2024-11-23 21:45:47 +0500

    Add files via upload

---
+++ HeroSection.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+const HeroSection = () => {
+  return (
+    <div>
+    <div className='flex'>
+    <div className='w-[1024px] flex flex-col justify-center items-start '>
+ <div className='w-[900px] h-[800px] mt-[141px] '> 
+        <div className=' flex  flex-col h-[300px] p-[0px 60px 0px 80px] gap-[24px] ml-[30px]  '>
+            <img src='/image/Column.svg' alt='text'/>
+            </div>
+            </div>
+            </div>
+         <div className='flex w-[600px] h-[900px]  '>
+         <img src="/image/image.svg" alt="model picture" />
+         </div>
+         </div>
+       
+        <div className=' w-[1280px] h-[228px]  ml-[100px] p-[80px 64px 80px 64px] gap-[64px] bg-[#F7F7F7]
+'>
+    <img src='/image/bar.svg' alt='bar'/>
+</div>
+
+      </div>
+
+   
+  )
+}
+
+export default HeroSection;
+
+
+
